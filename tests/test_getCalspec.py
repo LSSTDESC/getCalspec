cf04202e7e377e2b530c3c2c831ff83610906e28
Merlin Fisher-Levine <merlin.fisherlevine@gmail.com>
2022-06-02 07:00:04 -0500

    Add getting a table and a __main__ method to tests

--- tests/test_getCalspec.py
+++ tests/test_getCalspec.py
@@ -1,11 +1,21 @@
 import unittest
-from getCalspec import is_calspec
+from getCalspec import is_calspec, Calspec
+from astropy.table import Table as astropyTable
 
 
 class GetCalspecTestCase(unittest.TestCase):
     """A test case for the defect finding task."""
 
-    def testInit(self):
+    def test_is_calspec(self):
         self.assertTrue(is_calspec("eta1 dor"))
         self.assertFalse(is_calspec("NotACalspecStar"))
         self.assertFalse(is_calspec("Not A Calspec Star With Spaces"))
+
+    def test_Calspec(self):
+        c = Calspec('eta dor')
+        table = c.get_spectrum_table()
+        self.assertIsInstance(table, astropyTable)
+
+
+if __name__ == "__main__":
+    unittest.main()
